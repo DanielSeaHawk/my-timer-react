5f5692c589ad64baffe5872c3a4dd09455fd75d9
Daniel <yyc82663901@gmail.com>
2023-07-04 20:39:46 -0400

    Lifting state up

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import Log from "./Component/Logs/Log";
 import LogForm from "./Component/LogsForm/LogForm";
 import './App.css'
 
-export const App = () => {
+export const App = (props) => {
+  const logData = [
+    { id: "001", date: new Date(2021, 5, 30), desc: "React", time: 30 },
+    { id: "002", date: new Date(2022, 2, 1), desc: "Javascript", time: 130 },
+    { id: "003", date: new Date(2022, 9, 23), desc: "Vue", time: 45 },
+    { id: "004", date: new Date(2023, 7, 17), desc: "CSS", time: 90 },
+  ];
   return (
     <div className="app">
       <LogForm />
-      <Log />
+      <Log logData = {logData}/>
     </div>
   );
 };
--- src/Component/Logs/Log.js
+++ src/Component/Logs/Log.js
@@ -3,15 +3,10 @@ import LogItem from "./LogItem/LogItem";
 import "./Log.css";
 import Card from "../UI/Card/Card";
 
-export const Log = () => {
-  const logData = [
-    { id: "001", date: new Date(2021, 5, 30), desc: "React", time: 30 },
-    { id: "002", date: new Date(2022, 2, 1), desc: "Javascript", time: 130 },
-    { id: "003", date: new Date(2022, 9, 23), desc: "Vue", time: 45 },
-    { id: "004", date: new Date(2023, 7, 17), desc: "CSS", time: 90 },
-  ];
+export const Log = (props) => {
 
-  const logItemData = logData.map((item) => (
+
+  const logItemData = props.logData.map((item) => (
     <LogItem {...item} key={item.id} />
   ));
 
